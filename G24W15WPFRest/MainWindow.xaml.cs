0544787e5cd10363c6d8471303a837e9aa2d5462
권찬호 <rnjscksdud17@gmail.com>
2024-12-12 15:39:49 +0900

    초기 커밋.

---
+++ G24W15WPFRest/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace G24W15WPFRest;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private GameViewModel vm = new GameViewModel();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = vm; //반드시 연결, 시험출제 가능
+
+        GameGrid.ItemsSource = vm.Games;
+
+        InitializeGames();
+    }
+
+    private async void InitializeGames()
+    {
+        await vm.GetGames();
+    }
+
+    public void StartWebBrowser(object sender, EventArgs e)
+    {
+        string url = (string)((Button)sender).Tag;
+        Process.Start(
+            new ProcessStartInfo("cmd", $"/c start {url}")
+            {
+                CreateNoWindow = true
+            }
+        );
+    }
+}
